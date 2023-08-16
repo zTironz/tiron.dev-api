ef8b6ba3fd8eb5c2f02cab80f09919b0c05e1ef4
tiron <ole_g92@bk.ru>
2023-08-16 19:48:55 +0300

    some features to test api and front

--- app.js
+++ app.js
@@ -16,7 +16,20 @@ const app = express();
 
 app.use(helmet());
 app.use(limiter);
-app.use(cors());
+// app.use(cors({
+//   origin: "*",
+//   credentials: true,
+// }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.header('Origin'));
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
